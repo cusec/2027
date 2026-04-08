a3fdaa7c8c009facfee3118015127e4acf583b23
Angel Shinh <angelshinh2023@gmail.com>
2026-04-07 20:25:17 -0400

    refactor: unify locale path with messages lang

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -8,12 +8,9 @@ export default getRequestConfig(async ({requestLocale}) => {
   const locale = hasLocale(routing.locales, requested)
     ? requested
     : routing.defaultLocale;
-
-  // get the major language string, e.g, 'en', 'fr'
-  const language = locale.split('-')[0];
  
   return {
     locale,
-    messages: (await import(`../../messages/${language}.json`)).default
+    messages: (await import(`../../messages/${locale}.json`)).default
   };
 });
